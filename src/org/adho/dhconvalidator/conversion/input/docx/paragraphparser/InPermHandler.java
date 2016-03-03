d6872cdc6e98ef25714142b52c66c64547d64590
Marco Petris <marco.petris@web.de>
2016-03-03 22:29:57 +0100

    bugfix parsing editable and non editable sections in docx

--- src/org/adho/dhconvalidator/conversion/input/docx/paragraphparser/InPermHandler.java
+++ src/org/adho/dhconvalidator/conversion/input/docx/paragraphparser/InPermHandler.java
@@ -4,6 +4,8 @@
  */
 package org.adho.dhconvalidator.conversion.input.docx.paragraphparser;
 
+import org.adho.dhconvalidator.conversion.input.docx.DocxInputConverter.Namespace;
+
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.XPathContext;
@@ -20,7 +22,8 @@ public class InPermHandler implements StateHandler {
 			XPathContext xPathContext) {
 		
 		// are we still in an editable section?
-		if (matchElement.getLocalName().equals("permEnd")) { //$NON-NLS-1$
+		if (matchElement.getLocalName().equals("permEnd") //$NON-NLS-1$
+				|| matchElement.getFirstChildElement("permEnd", Namespace.MAIN.toUri()) != null) { //$NON-NLS-1$
 			// editable section ends here, start searching for the next one
 			return State.SEEKPERMSTART;
 		}
